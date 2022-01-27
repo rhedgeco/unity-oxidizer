abcfd69e82c9b38400ee47f04dcb061e3abb0be8
rhedgeco <rhedgeco@gmail.com>
2022-01-27 10:47:34 -0800

    updated oxidizer usage with basic methods

--- RustWorkspace/unity_oxidizer_usage/src/lib.rs
+++ RustWorkspace/unity_oxidizer_usage/src/lib.rs
@@ -1,6 +1,21 @@
+use unity_oxidizer::collections::rusty_array::RustyArray;
 use unity_oxidizer::collections::rusty_list::RustyList;
 
 #[no_mangle]
-pub extern "C" fn test_array_populate(&mut RustyArray<f32>) {
+pub extern "C" fn test_array_populate(rusty_array: &mut RustyArray<f32>) {
+    let array = rusty_array.get_array();
 
+    for i in 0..array.len() {
+        array[i] = i as f32;
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn test_list_populate(rusty_array: &mut RustyList<f32>) {
+    let mut list = rusty_array.get_list_handler();
+    list.clear();
+
+    for i in 0..list.length() {
+        list.add(i as f32);
+    }
 }
